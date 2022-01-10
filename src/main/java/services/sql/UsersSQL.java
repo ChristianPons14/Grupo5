3f3f68b61f00f95efdcb871bf04ba949ebb47e91
ezequielhidalgoponce <ezequielhidalgoponce.sanjose@alumnado.fundacionloyola.net>
2022-01-10 17:54:49 +0100

    Modificaciones en UserSQL

--- src/main/java/services/sql/UsersSQL.java
+++ src/main/java/services/sql/UsersSQL.java
@@ -3,12 +3,44 @@
  */
 package services.sql;
 
+import java.sql.ResultSet;
+
+import services.conector.Conector;
+
 /**
  * @author ezequ
  *
  */
 public class UsersSQL {
-	public UsersSQL() {
+		Conector c = new Conector();
+		ResultSet almacenador;
+		private String userName;
+		private String password;
 
+		public UsersSQL(Vlogin window) {
+
+			userName = window.getUserName().getText();
+			password = window.getPassword().getText();
+			System.out.println(checkLogin());
+
+		}
+
+		private boolean checkLogin() {
+			almacenador = c.crearSQL(
+					"SELECT * FROM USERS WHERE user_name like '" + userName + "' and user_password like '" + password + "';");
+
+			try {
+				if (almacenador.next()) {
+					System.out.println("El usuario y password son correctos");
+					return true;
+				} else {
+					System.out.println("El usuario y password son incorrectos");
+					return false;
+				}
+			} catch (SQLException e) { // TODO Auto-generated catch block e.printStackTrace(); }
+
+			}
+			return false;
+		
 	}
 }
