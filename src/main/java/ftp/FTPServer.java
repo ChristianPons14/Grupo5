9dfdd61a9637d162096e01789116289a6956cab4
MiguelRuizP <miguelruizpryshlyak.sanjose@alumnado.fundacionloyola.net>
2022-01-14 00:46:12 +0100

    Creadas clases base del servidor y cliente FTP

---
+++ src/main/java/ftp/FTPServer.java
@@ -0,0 +1,85 @@
+package ftp;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
+
+import services.conector.Conector;
+import services.sql.User;
+import services.sql.UsersSQL;
+
+/**
+ * 
+ * @author Miguel Ruiz
+ *
+ */
+public class FTPServer {
+
+	public static final int PORT = 5000;
+	public static final String directory = "C:\\Users\\Mig27\\Desktop\\ftpcolumbia";
+
+	private static FtpServer ftpServer;
+	
+	public static void main(String[] args) {
+		initServer();
+	}
+
+	public static void initServer() {
+		try {
+			// utilizado para crear servidor
+			FtpServerFactory serverFactory = new FtpServerFactory();
+			// Información de configuración, como configurar el puerto de escucha y el
+			// filtro IP
+			ListenerFactory listenerFactory = new ListenerFactory();
+			// establece el puerto
+			listenerFactory.setPort(PORT);
+			// Cerrar la conexión si no se han realizado operaciones durante cinco minutos
+			// listenerFactory.setIdleTimeout(5*60*1000);
+
+			List<Authority> authorities = new ArrayList<Authority>();
+			authorities.add(new WritePermission());
+			
+			// Establecer nombre de usuario y contraseña
+			List<User> usuarios = UsersSQL.retrieveAllUsersPassword(new Conector().getMySQLConnection());
+			usuarios.forEach(user -> {
+				BaseUser userFtp = new BaseUser();
+				userFtp.setName(user.getUserName());
+				userFtp.setPassword(user.getPassword());
+	 
+				if(!new File(directory + "/" + user.getUserName()).exists()) {
+					new File(directory + "/" + user.getUserName()).mkdirs();
+				}
+				
+				if(user.isDirective()) {
+					userFtp.setHomeDirectory(directory);
+				}
+				else {
+					userFtp.setHomeDirectory(directory + "/" + user.getUserName());
+				}
+				
+				userFtp.setAuthorities(authorities);
+				try {
+					serverFactory.getUserManager().save(userFtp);
+				} catch (FtpException e) {
+					e.printStackTrace();
+				}
+			});
+			
+			serverFactory.addListener("default", listenerFactory.createListener());
+
+			ftpServer = serverFactory.createServer();
+			ftpServer.start();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/ftp/FtpClient.java
@@ -0,0 +1,179 @@
+package ftp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+/**
+ * 
+ * @author Miguel Ruiz
+ *
+ */
+public class FtpClient {
+
+	public static final String HOST = "localhost";
+	public static final int PORT = 5000;
+	public static final String USERNAME = "asdf";
+	public static final String PASSWORD = "12345";
+	
+	public static FTPClient client;
+
+	public static void main(String[] args) {
+		try {
+			client = conectarFtp(HOST, PORT, USERNAME, PASSWORD);
+			Arrays.asList(client.listFiles()).forEach(str -> System.out.println(str.toString()));
+			System.out.println(client.getReplyString());
+		} catch (IOException e) {
+			System.err.println("Error al conectar...");
+		}
+	}
+
+	/**
+	 * Conecta al servidor FTP con el usuario especificado
+	 * 
+	 * @param hostname - String - dirección del servidor FTP
+	 * @param port - int - Número de puerto del servidor FTP
+	 * @param nombre - String - de usuario FTP cuenta de inicio de sesión
+	 * @param contraseña - String - Contraseña de inicio de sesión FTP
+	 * @return cliente FTP si hace login correcto
+	 * @throws IOException
+	 */
+	public static FTPClient conectarFtp(String hostname, int port, String username, String password)
+			throws IOException {
+		FTPClient ftpClient = new FTPClient();
+		ftpClient.connect(hostname, port);
+		ftpClient.login(username, password);
+		int replyCode = ftpClient.getReplyCode();
+		if (!FTPReply.isPositiveCompletion(replyCode)) {
+			return null;
+		} else {
+			return ftpClient;
+		}
+	}
+
+	/**
+	 * Subir archivo (disponible para la capa Acción / Controlador)
+	 * 
+	 * @param ftpClient - FTPClient - Cliente FTP que se conecta
+	 * @param pathname - String - Servidor FTP guardar directorio
+	 * @param fileName - String - Nombre del archivo después de subirlo al servidor FTP
+	 * @param inputStream - InputStream - flujo de archivo de entrada
+	 * @return
+	 */
+	public static boolean uploadFile(FTPClient ftpClient, String pathname, String fileName, InputStream inputStream) {
+		boolean flag = false;
+		ftpClient.setControlEncoding("UTF-8");
+		try {
+			ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+			ftpClient.makeDirectory(pathname);
+			ftpClient.changeWorkingDirectory(pathname);
+			ftpClient.storeFile(fileName, inputStream);
+			inputStream.close();
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	/**
+	 * Subir archivo (puede cambiar el nombre del archivo)
+	 * 
+	 * @param ftpClient - FTPClient - Cliente FTP que se conecta
+	 * @param pathname - String - Servidor FTP guardar directorio
+	 * @param filename - String - Nombre del archivo después de subirlo al servidor FTP
+	 * @param originfilename - String - el nombre del archivo a cargar (dirección absoluta)
+	 * @return
+	 */
+	public static boolean uploadFileFromProduction(FTPClient ftpClient, String pathname, String filename,
+			String originfilename) {
+		boolean flag = false;
+		try {
+			InputStream inputStream = new FileInputStream(new File(originfilename));
+			flag = uploadFile(ftpClient, pathname, filename, inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	/**
+	 * Subir archivo (no se permite cambiar el nombre del archivo)
+	 * 
+	 * @param ftpClient - FTPClient - Cliente FTP que se conecta
+	 * @param pathname - String - Servidor FTP guardar directorio
+	 * @param originfilename - String - el nombre del archivo a cargar (dirección absoluta)
+	 * @return
+	 */
+	public static boolean uploadFileFromProduction(FTPClient ftpClient, String pathname, String originfilename) {
+		boolean flag = false;
+		try {
+			String fileName = new File(originfilename).getName();
+			InputStream inputStream = new FileInputStream(new File(originfilename));
+			flag = uploadFile(ftpClient, pathname, fileName, inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	/**
+	 * Eliminar archivos
+	 * 
+	 * @param ftpClient - FTPClient - Cliente FTP que se conecta
+	 * @param pathname - String - Servidor FTP guardar directorio
+	 * @param filename - String - el nombre del archivo a eliminar
+	 * @return
+	 */
+	public static boolean deleteFile(FTPClient ftpClient, String pathname,
+			String filename) {
+		boolean flag = false;
+		try {
+			// Cambiar directorio FTP
+			ftpClient.changeWorkingDirectory(pathname);
+			ftpClient.dele(filename);
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	/**
+	 * Descargar archivo
+	 * 
+	 * @param ftpClient - FTPClient - Cliente FTP que se conecta
+	 * @param pathname - String - Directorio de archivos del servidor FTP
+	 * @param nombre - String - de archivo nombre de archivo
+	 * @param localpath - String - Ruta del archivo después de la descarga
+	 * @return
+	 */
+	public static boolean downloadFile(FTPClient ftpClient, String pathname,
+			String filename, String localpath) {
+		boolean flag = false;
+		try {
+			ftpClient.changeWorkingDirectory(pathname);
+			FTPFile[] ftpFiles = ftpClient.listFiles();
+			for (FTPFile file : ftpFiles) {
+				if (filename.equalsIgnoreCase(file.getName())) {
+					File localFile = new File(localpath + "/" + file.getName());
+					OutputStream os = new FileOutputStream(localFile);
+					ftpClient.retrieveFile(file.getName(), os);
+					os.close();
+				}
+			}
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+}
