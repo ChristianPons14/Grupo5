d080ede2c0f0e0f568ea91c42af8e48cefcbd222
MiguelRuizP <miguelruizpryshlyak.sanjose@alumnado.fundacionloyola.net>
2022-01-14 00:45:28 +0100

    Comento bien los métodos del UsersSQL

--- src/main/java/services/sql/UsersSQL.java
+++ src/main/java/services/sql/UsersSQL.java
@@ -14,15 +14,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Contiene los métodos de acceso a la tabla users de la base de datos MySQL
  * @author Grupo 5
- *
  */
 public class UsersSQL {
-	/*
-	 * private String userName; private String userSurName; private String password;
-	 * private boolean isDirective;
-	 */
 
+	/**
+	 * Comprueba el login, y devuelve true si ha iniciado sesion correctamente
+	 * 
+	 * @param con - Connection - Conexion MySQL
+	 * @param userName - String - Nombre del usuario
+	 * @param password - String - Contraseñadel usuario a iniciar sesion
+	 * @return true si inicia correctamente, false si no inicia, o hay error
+	 */
 	public static User checkLogin(Connection con, String userName, String password) {
 		String sql = "SELECT ID, USER_NAME, USER_SURNAMES, DIRECTIVE, SIGN_IN FROM USERS WHERE USER_NAME like ? AND USER_PASSWORD LIKE ?";
 
@@ -36,7 +40,7 @@ public class UsersSQL {
 				throw new SQLException();
 			}
 			user = new User(almacenador.getInt(1), almacenador.getString(2), almacenador.getString(3),
-					almacenador.getInt(4), almacenador.getDate(5));
+					almacenador.getBoolean(4), almacenador.getDate(5));
 			
 			prepStmt.close();
 			almacenador.close();
@@ -48,6 +52,16 @@ public class UsersSQL {
 		}	
 	}
 
+	/**
+	 * Crea un usuario, con los parametros especificados
+	 * 
+	 * @param con - Connection - Conexion con MySQL
+	 * @param userName - String - nombre de usuario
+	 * @param userSurnames - String - apellidos de l usuario
+	 * @param password - String - contraseña del usuario
+	 * @param isDirective - boolean - es true si es directivo y tiene permisos especiales
+	 * @return true si crea con exito, o false si no tiene exito
+	 */
 	public static boolean createUser(Connection con, String userName, String userSurnames, String password,
 			boolean isDirective) {
 		String sql = "INSERT INTO users VALUES (?,?,?,?,?)";
@@ -76,6 +90,12 @@ public class UsersSQL {
 
 	}
 	
+	/**
+	 * Devuelve la contraseña del usuario especificado
+	 * @param con - Connection - Conexion con MySQL
+	 * @param userName - String - usuario a buscar
+	 * @return String - contraseña del usuario
+	 */
 	public static String getUserPassword(Connection con, String userName) {
 		String password = "";
 		
@@ -95,16 +115,49 @@ public class UsersSQL {
 		return password;	
 	}
 	
+	/**
+	 * Devuelve a todos los usuarios y sus datos, incluyendo la contraseña
+	 * 
+	 * @param con - Connection - Conexion MySQL
+	 * @return lista con todos los usuarios
+	 */
+	public static List<User> retrieveAllUsersPassword(Connection con) {
+		List<User> arrayUsers = new ArrayList<User>();
+		try {
+			PreparedStatement ps = con
+					.prepareStatement("SELECT ID, USER_NAME, USER_SURNAMES, USER_PASSWORD, DIRECTIVE, SIGN_IN FROM USERS");
+			ResultSet almacenador = ps.executeQuery();
+
+			while (almacenador.next()) {
+				User usuarios = new User(almacenador.getInt(1), almacenador.getString(2), almacenador.getString(3),
+						almacenador.getString(4), almacenador.getBoolean(5), almacenador.getDate(6));
+				arrayUsers.add(usuarios);
+			}
+			
+			ps.close();
+			almacenador.close();
+		} catch (SQLException e) {
+
+		}
+		return arrayUsers;
+	}
+	
+	/**
+	 * Devuelve todos los datos de todos los usuarios, quitando la contraseña
+	 * 
+	 * @param con - Connection - Conexion MySQL
+	 * @return Lista con todos los usuarios
+	 */
 	public static List<User> retrieveAllUsers(Connection con) {
 		List<User> arrayUsers = new ArrayList<User>();
 		try {
 			PreparedStatement ps = con
 					.prepareStatement("SELECT ID, USER_NAME, USER_SURNAMES, DIRECTIVE, SIGN_IN FROM USERS");
 			ResultSet almacenador = ps.executeQuery();
-
+			
 			while (almacenador.next()) {
 				User usuarios = new User(almacenador.getInt(1), almacenador.getString(2), almacenador.getString(3),
-						almacenador.getInt(4), almacenador.getDate(5));
+						almacenador.getBoolean(4), almacenador.getDate(5));
 				arrayUsers.add(usuarios);
 			}
 			
